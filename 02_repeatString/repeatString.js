b7617dc963dcd9937792f3e021c028459cb6d3b5
Kristopher Gates <kdlgates@kdlgates.com>
2023-08-25 13:08:10 -0400

    Complete 'helloWorld', 'repeatString'

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, repeatCount) {
+    if (repeatCount < 0) {
+        return "ERROR";
+    }
+    outputStr = "";
+    for (let i = 0; i < repeatCount; i++) {
+        outputStr += str;
+    }
+    return outputStr;
 };
 
 // Do not edit below this line
