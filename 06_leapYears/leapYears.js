8357eed5b41bd1e290f1523095eaffd2bd02b714
Kristopher Gates <kdlgates@kdlgates.com>
2023-09-22 09:44:30 -0400

    Solve 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 400) {
+        return true;
+    } else if (year % 100) {
+        return false;
+    } else if (year % 4) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
