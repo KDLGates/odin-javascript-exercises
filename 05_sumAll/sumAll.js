1b6b2ac9752939de7b31e4310cd069080c5721f0
Kristopher Gates <kdlgates@kdlgates.com>
2023-09-22 09:44:16 -0400

    Solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(n1, n2) {
+    if (n2 > n1) {
+        let sum = 0;
+        for (let i = n1; i <= n2; i++) {
+            sum += i;
+        }
+        return sum;
+    } else if (n1 > n2) {
+        let sum = 0;
+        for (let i = n2; i <= n1; i++) {
+            sum += i;
+        }
+        return sum;
+    } else if (n1 == n2) {
+        return 0;
+    }
 };
 
 // Do not edit below this line
