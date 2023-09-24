19d466dccdbfed038696faebadf01d6d34d40c6a
Kristopher Gates <kdlgates@kdlgates.com>
2023-09-24 14:43:45 -0400

    Solve 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest = 0;
+    for (person of people) {
+        if (!person.yearOfDeath) {
+            person.yearOfDeath = new Date().getFullYear();
+        }
+        if (person.yearOfDeath - person.yearOfBirth > oldest) {
+            oldest = person.yearOfDeath - person.yearOfBirth;
+            oldestPerson = person;
+        }
+    }
+    return oldestPerson;
 };
 
 // Do not edit below this line
