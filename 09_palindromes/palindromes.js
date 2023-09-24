d73536b76a9717d905d36a6aa339880a7e76d489
Kristopher Gates <kdlgates@kdlgates.com>
2023-09-24 13:04:37 -0400

    Solve 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let normalizedStr = str.replace(/[,.!? ]/g, '').toLowerCase();
+    let reversedStr = normalizedStr.split('').reverse().join('');
+    if (normalizedStr === reversedStr) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
