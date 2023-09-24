e09c12569700ec7f19fa30ad54c6ec614a6dfcdb
Kristopher Gates <kdlgates@kdlgates.com>
2023-09-24 14:22:45 -0400

    Solve 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let titles = [];
+    for (book of books) {
+        if ('title' in book) {
+            titles.push(book.title);
+        }
+    }
+    return titles;
 };
 
 // Do not edit below this line
